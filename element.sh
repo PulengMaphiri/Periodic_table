edbe5c14b3256defb8a7656f623a02fdd57ef6ca
Puleng Maphiri <maphiripn@gmail.com>
2024-09-09 19:12:55 +0000

    chore: deleting a non exixtent element

--- element.sh
+++ element.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
 
-if [[ -z $1 ]];then
+if [[ $1 ]]
+  then
+  if [[ ! $1 =~ ^[0-9]+$ ]]
+  then
+  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+  else
+  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number=$1")
+  fi
+    if [[ -z $ELEMENT ]]
+    then
+      echo "I could not find that element in the database."
+    else
+      echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MPC BPC SYMBOL NAME TYPE
+      do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
+      done
+    fi
+  else
   echo "Please provide an element as an argument."
-  exit
 fi
 
-if [[ $1 =~ ^[0-9]+$ ]];then
-  ELEMENT=$($PSQL "SELECT atomic_number,atomic_mass,symbol,name,type,melting_point_celsius,boiling_point_celsius FROM elements LEFT JOIN properties USING (atomic_number) LEFT JOIN types USING (type_id) WHERE atomic_number=$1")
-else
-  ELEMENT=$($PSQL "SELECT atomic_number,atomic_mass,symbol,name,type,melting_point_celsius,boiling_point_celsius FROM elements LEFT JOIN properties USING (atomic_number) LEFT JOIN types USING (type_id) WHERE symbol = '$1' OR name = '$1'")
-fi
-
-if [[ -z $ELEMENT ]];then
-  echo "I could not find that element in the database."
-  exit
-fi
-
-echo $ELEMENT | while IFS=" |" read A_NUMBER A_MASS SYMBOL NAME TYPE M_POINT B_POINT ; do
-  echo "The element with atomic number $A_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $A_MASS amu. $NAME has a melting point of $M_POINT celsius and a boiling point of $B_POINT celsius."
-done 
